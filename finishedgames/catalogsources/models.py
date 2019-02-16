9d1a5c286454f8712e0cb9ec70b9d6e506687ec8
Kartones <diego@kartones.net>
2019-02-16 12:38:35 +0100

    FK from fetched item to FG corresponding

--- finishedgames/catalogsources/models.py
+++ finishedgames/catalogsources/models.py
@@ -4,7 +4,7 @@ from typing import Any
 
 from django.db import models
 
-from core.models import (BaseGame, BasePlatform)
+from core.models import (BaseGame, BasePlatform, Game, Platform)
 
 
 class FetchedGame(BaseGame):
@@ -17,6 +17,7 @@ class FetchedGame(BaseGame):
     source_url = models.CharField("Resource source URI", max_length=255)
     change_hash = models.CharField("Marker to detect data changes after a fetch", max_length=32)
     hidden = models.BooleanField("Item hidden", default=False, db_index=True)
+    fg_game_id = models.ForeignKey(Game, on_delete=models.SET_NULL, null=True, default=None, blank=True)
     # Override parent FK
     parent_game = models.ForeignKey("FetchedGame", on_delete=models.CASCADE, null=True, default=None, blank=True)
 
@@ -59,6 +60,7 @@ class FetchedPlatform(BasePlatform):
     source_url = models.CharField("Resource source URI", max_length=255)
     change_hash = models.CharField("Marker to detect data changes after a fetch", max_length=32)
     hidden = models.BooleanField("Item hidden", default=False, db_index=True)
+    fg_platform_id = models.ForeignKey(Platform, on_delete=models.SET_NULL, null=True, default=None, blank=True)
 
     def save(self, *args: Any, **kwargs: Any) -> None:
         if not self.shortname:
