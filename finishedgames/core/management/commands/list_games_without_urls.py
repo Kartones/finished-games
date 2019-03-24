3b200bff9de9111fcc7ad03cda48406b6cc5b6e2
Kartones <diego@kartones.net>
2019-03-24 23:10:42 +0100

    management command to list Games without urls

---
+++ finishedgames/core/management/commands/list_games_without_urls.py
@@ -0,0 +1,34 @@
+from typing import (Any, Dict)
+
+from django.conf import settings
+from django.core.management.base import (BaseCommand, CommandParser)
+from django.urls import reverse
+
+from core.models import Game
+
+
+class Command(BaseCommand):
+    help = "Lists all Games with 'urls' field unpopulated"
+
+    def add_arguments(self, parser: CommandParser) -> None:
+        pass
+
+    def handle(self, *args: Any, **options: Dict) -> None:
+        games = Game.objects \
+                    .filter(urls="")
+
+        host = settings.ALLOWED_HOSTS[-1]
+        if settings.DEBUG:
+            protocol = "http"
+            # NOTE: This is the same port configured in Docker
+            port = ":5000"
+        else:
+            protocol = "https"
+            # assumes 443
+            port = ""
+
+        for game in games:
+            url = "{protocol}://{host}{port}{path}".format(
+                protocol=protocol, host=host, port=port, path=reverse("admin:core_game_change", args=[game.id])
+            )
+            self.stdout.write("{} - {}".format(game.name, url))
