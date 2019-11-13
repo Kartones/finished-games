32ec9f83f1a968f68688d62c67a0a3f1b3f64a34
Kartones <diego@kartones.net>
2019-11-13 23:48:14 +0100

    setting up pre-commit

--- finishedgames/test/test_formatters.py
+++ finishedgames/test/test_formatters.py
@@ -5,16 +5,17 @@ SOURCE_FOLDER = "."
 
 
 def test_isort_compliance() -> None:
-    binary_location = subprocess.check_output("which isort",
-                                              shell=True,
-                                              stderr=sys.stderr).decode("ascii").replace("\n", "")
+    binary_location = (
+        subprocess.check_output("which isort", shell=True, stderr=sys.stderr).decode("ascii").replace("\n", "")
+    )
     print("")
-    result = subprocess.check_output("{} -rc --atomic {}".format(binary_location, SOURCE_FOLDER),
-                                     shell=True,
-                                     stderr=sys.stderr).decode("utf-8")
+    result = (
+        subprocess.check_output(
+            "{} -rc --atomic {}".format(binary_location, SOURCE_FOLDER), shell=True, stderr=sys.stderr
+        )
+        .decode("ascii")
+        .replace("\n", "")
+    )
 
     print(result)
-    assert all([
-        "Fixing " not in result,
-        "ERROR: " not in result,
-    ]), result
+    assert all(["Fixing " not in result, "ERROR: " not in result]), result
--- finishedgames/test/test_linters.py
+++ finishedgames/test/test_linters.py
@@ -10,24 +10,25 @@ Both pytest-mypy and pytest-flake8 exist, but need more fiddling with config fil
 
 
 def test_flake8_compliance() -> None:
-    binary_location = subprocess.check_output("which flake8",
-                                              shell=True,
-                                              stderr=sys.stderr).decode("ascii").replace("\n", "")
-
-    result = subprocess.call("{} {}".format(binary_location, SOURCE_FOLDER),
-                             shell=True,
-                             stdout=sys.stdout,
-                             stderr=sys.stderr)
+    binary_location = (
+        subprocess.check_output("which flake8", shell=True, stderr=sys.stderr).decode("ascii").replace("\n", "")
+    )
+    print("")
+    result = subprocess.call(
+        "{} {}".format(binary_location, SOURCE_FOLDER), shell=True, stdout=sys.stdout, stderr=sys.stderr
+    )
     assert result == 0
 
 
 def test_mypy_compliance() -> None:
-    binary_location = subprocess.check_output("which mypy",
-                                              shell=True,
-                                              stderr=sys.stderr).decode("ascii").replace("\n", "")
+    binary_location = (
+        subprocess.check_output("which mypy", shell=True, stderr=sys.stderr).decode("ascii").replace("\n", "")
+    )
     print("")
-    result = subprocess.call("{} --config-file ../mypy.ini {}".format(binary_location, SOURCE_FOLDER),
-                             shell=True,
-                             stdout=sys.stdout,
-                             stderr=sys.stderr)
+    result = subprocess.call(
+        "{} --config-file ../mypy.ini {}".format(binary_location, SOURCE_FOLDER),
+        shell=True,
+        stdout=sys.stdout,
+        stderr=sys.stderr,
+    )
     assert result == 0
