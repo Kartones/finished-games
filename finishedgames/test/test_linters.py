ebe8abb1bac38d7769c4fb0d84a0b84ac0f4eb8d
Kartones <diego@kartones.net>
2019-11-13 23:53:07 +0100

    move isort, flake8 and black to pre-commit

--- finishedgames/test/test_linters.py
+++ finishedgames/test/test_linters.py
@@ -5,19 +5,8 @@ SOURCE_FOLDER = "."
 
 
 """
-Both pytest-mypy and pytest-flake8 exist, but need more fiddling with config files than this test-based linting
+pytest-mypy exists, but needs more fiddling with config files than this test-based linting
 """
-
-
-def test_flake8_compliance() -> None:
-    binary_location = (
-        subprocess.check_output("which flake8", shell=True, stderr=sys.stderr).decode("ascii").replace("\n", "")
-    )
-    print("")
-    result = subprocess.call(
-        "{} {}".format(binary_location, SOURCE_FOLDER), shell=True, stdout=sys.stdout, stderr=sys.stderr
-    )
-    assert result == 0
 
 
 def test_mypy_compliance() -> None:
