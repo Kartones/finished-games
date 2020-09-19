f1dad633f60678b82fdd69e1b6791422730d179f
Kartones <diego@kartones.net>
2020-09-19 09:07:15 +0200

    Add searchable name to Games for searching + populate script

---
+++ finishedgames/core/management/commands/recreate_game_names_for_search.py
@@ -0,0 +1,28 @@
+from typing import Any, Dict
+
+from core.models import Game
+from django.core.management.base import BaseCommand, CommandParser
+
+
+class Command(BaseCommand):
+    help = "(Re)Creates all Game searchable names"
+
+    def add_arguments(self, parser: CommandParser) -> None:
+        pass
+
+    def handle(self, *args: Any, **options: Dict) -> None:
+        games_count = Game.objects.count()
+        games = Game.objects.all()
+
+        counter = 0
+
+        self.stdout.write("Recreating searchable names for Games")
+
+        for game in games:
+            game.save()
+            counter += 1
+            self.stdout.write(".", ending="")
+            if counter % 50 == 0:
+                self.stdout.write(" {:.3f}%".format(counter * 100 / games_count))
+
+        self.stdout.write("\nFinished")
