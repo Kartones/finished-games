0d0ab3b9f91419e45a18031e2639323467135b3f
Kartones <diego@kartones.net>
2023-09-24 18:18:52 +0200

    fix: local settings should override dev settings

--- finishedgames/finishedgames/settings/dev.py
+++ finishedgames/finishedgames/settings/dev.py
@@ -4,12 +4,6 @@ import os
 
 from .base import *  # NOQA: F401, F403
 
-# To mess around with configuration without ever commiting it to the repository (but neither using prod settings)
-try:
-    from .local import *  # NOQA: F401, F403
-except Exception:  # nosec
-    pass
-
 DEBUG = True
 
 # To disable it even in development
@@ -45,3 +39,11 @@ STATICFILES_DIRS = [
 EXTRA_GAME_INFO_BUTTONS += [  # NOQA: F405
     ("PCGamingWiki", "https://www.pcgamingwiki.com/w/index.php?search={}&title=Special%3ASearch", 10),
 ]
+
+
+# To mess around with configuration without ever commiting it to the repository (but neither using prod settings)
+# NOTE: Should go at the end of the file, to override any dev settings
+try:
+    from .local import *  # NOQA: F401, F403
+except Exception:  # nosec
+    pass
