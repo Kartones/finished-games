0bffb857c26077266646dc0fe78e6839a4dfe197
Kartones <diego@kartones.net>
2026-01-02 20:24:24 +0100

    feat: SteamAdapter to fetch owned games

--- finishedgames/catalogsources/management/helpers.py
+++ finishedgames/catalogsources/management/helpers.py
@@ -3,19 +3,22 @@ from typing import Type
 
 from catalogsources.adapters.base_adapter import BaseAdapter
 from catalogsources.adapters.giant_bomb_adapter import GiantBombAdapter
+from catalogsources.adapters.steam_adapter import SteamAdapter
+
+
+def source_class_from_id(source_id: str) -> Type[BaseAdapter]:
+    if source_id == GiantBombAdapter.source_id():
+        return GiantBombAdapter
+    if source_id == SteamAdapter.source_id():
+        return SteamAdapter
+
+    raise ValueError("Unknown source id '{}'".format(source_id))
 
 
 def wait_if_needed(time_elapsed: float) -> None:
     # Most apis allow 1 request per second, so default to 1 fetch request per second
     if time_elapsed < 1.0:
         time.sleep(1.0 - time_elapsed)
-
-
-def source_class_from_id(source_id: str) -> Type[BaseAdapter]:
-    if source_id == GiantBombAdapter.source_id():
-        return GiantBombAdapter
-
-    raise ValueError("Unknown source id '{}'".format(source_id))
 
 
 class TimeProfiler:
@@ -27,6 +30,6 @@ class TimeProfiler:
         self.start = time.perf_counter() if self.use_performance_counter else time.time()
         return self
 
-    def __exit__(self, *args: str) -> None:
+    def __exit__(self, *_: str) -> None:
         self.end = time.perf_counter() if self.use_performance_counter else time.time()
         self.duration = self.end - self.start
